0a24ea5e8f966390347310eb16cf5a4de3296cc3
Raya809 <diagomarinir@gmail.com>
2025-03-22 12:58:39 +0700

    pertemuan 2

---
+++ hello-react/src/App.js
@@ -0,0 +1,18 @@
+// import libraries
+import React from 'react';
+import Header from './Header';
+import Footer from './footer';
+// create component 
+
+const app = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>My Name is Diago </p>
+      <Header />
+      <Footer />
+    </div>
+  );
+} 
+// export component
+export default app;
---
+++ hello-react/src/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+return (
+    <div>
+<h3> Ini Halaman Footer </h3>
+<footer> Design by diago🤺 </footer>
+    </div>
+);    
+}
+export default Footer;
